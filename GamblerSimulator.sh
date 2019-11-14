28f30b33b00500e3358ff26de245b4886018b84a
Sheetal Bedarkar <sheetalbedarkar96@gmail.com>
2019-11-14 18:14:56 +0530

    total amount won or lost for 20 days

--- GamblerSimulator.sh
+++ GamblerSimulator.sh
@@ -1,24 +1,39 @@
 #!/bin/bash -x
 STAKE=100
 BET=1
-amount=0
-totalAmount=$STAKE
+totalAmountOf20Days=0
 
-while [ $totalAmount -le 150 -a $totalAmount -ge 50 ]
+for day in {1..20}
 do
-randomCheck=$((RANDOM%2))
+totalAmount=$STAKE
+amount=0
+winAmount=0
+lostAmount=0
 
-if [ $randomCheck -eq 0 ]
-then
-	echo "lost"
-	amount=$(($amount-1))
-	totalAmount=$(($STAKE+$amount))
+	while [ $totalAmount -le 105 -a $totalAmount -ge 95 ]
+	do
+	randomCheck=$((RANDOM%2))
 
-elif [ $randomCheck -eq 1 ] 
-then
-	amount=$(($amount+1))
-	echo "win"
-	totalAmount=$(($STAKE+$amount))
-fi
+		if [ $randomCheck -eq 0 ]
+		then
+			message="You Lost"
+			amount=$(($amount-1))
+			totalAmount=$(($STAKE+$amount))
+			lostAmount=$(($lostAmount-1))
+
+		elif [ $randomCheck -eq 1 ] 
+		then
+			message="You Win"
+			amount=$(($amount+1))
+			totalAmount=$(($STAKE+$amount))
+			winAmount=$(($winAmount+1))
+		fi
+
+	done
+	message="You are done for the day. Please come on next day"
+	echo "$day"
+	echo "$winAmount"
+	echo "$lostAmount"
+	amountOnDay=$(($winAmount-$lostAmount))
+	totalAmountOf20Days=$(($totalAmountOf20Days + $amount))
 done
-
